a4a91649d61e25c2e4e7b4338ffe1f96d1ccc4d6
NANMARAN2006-Ramesh <nmaran294@gmail.com>
2026-01-24 20:50:58 +0530

    ChangVarValue

--- ChangVarValue.c
+++ ChangVarValue.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main(){
-    int a=10;
+    // change variable value 1
+    int a = 10;
     a=20;
     printf("%d",a);
 
+    // Chnge variable value testcase 2
+    
+    a = 100;
+    printf("%d",a);
     return 0;
+
 }
