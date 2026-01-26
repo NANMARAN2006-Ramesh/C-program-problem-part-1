a527e3037dd13eb7ac4336f802f332873070d44f
NANMARAN2006-Ramesh <nmaran294@gmail.com>
2026-01-26 11:04:02 +0530

    Divisible By 11

---
+++ divideBy11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int ch;
+    scanf("%d",&ch);
+    if(ch%11==0){
+        Printf("Divisible By 11");
+    }
+    else{
+        Printf("Not Divisible By 11");
+    }
+
+    return 0;
+}
