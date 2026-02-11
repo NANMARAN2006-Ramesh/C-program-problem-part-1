3502897e2926b1b4942ca55998af152e26ec73f4
Nanmaran Ramesh <nmaran294@gmail.com>
2026-02-11 11:47:28 +0530

    Function Problem

---
+++ Function-1/function12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int leapyear(int year){
+    if((year%4==0 && year%100!=0) || year%400==0){
+        printf("%d is a leap year",year);
+    }
+    else{
+        printf("%d is not a leap year",year);
+    }
+}
+int main(){
+    int year;
+    printf("Enter a year : ");
+    scanf("%d",&year);
+    leapyear(year);
+    return 0;
+}
